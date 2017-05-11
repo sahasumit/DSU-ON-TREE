861bccff3f95ac4435d9a93b9c07eec0f04a6ba5
Sumit Saha <sahasumit288@gmail.com>
2017-05-11 07:26:14 +0600

    LCA FUNCTION ADDED

--- CODEFORCES/CF 130/E.cxx
+++ CODEFORCES/CF 130/E.cxx
@@ -102,18 +102,103 @@ void input()
     int q;
     cin>>q;
     int a,b;
-    FOR0(i,q){
+    FOR0(i,q)
+    {
 
-    cin>>a>>b;
-    Q.PB(MP(a,b));
+        cin>>a>>b;
+        Q.PB(MP(a,b));
     }
 
 
 }
+
+//lca fuctino
+
+int L[mx]; //লেভেল
+int P[mx][22]; //স্পার্স টেবিল
+int T[mx]; //প্যারেন্ট
+bool vis[mx];
+
+void dfs(int u,int par,int depth)
+{
+    vis[u]=1;
+    L[u]=depth;
+    T[u]=par;
+
+    for(int i=0; i<G[u].size(); i++)
+    {
+
+
+        int v=G[u][i];
+        if(v==par)continue;
+        dfs(v,u,depth+1);
+
+
+    }
+
+
+}
+
+int lca_query(int N, int p, int q) //N=নোড সংখ্যা
+{
+    int tmp, log, i;
+
+    if (L[p] < L[q])
+        tmp = p, p = q, q = tmp;
+
+    log=1;
+    while(1)
+    {
+        int next=log+1;
+        if((1<<next)>L[p])break;
+        log++;
+
+    }
+
+    for (i = log; i >= 0; i--)
+        if (L[p] - (1 << i) >= L[q])
+            p = P[p][i];
+
+    if (p == q)
+        return p;
+
+    for (i = log; i >= 0; i--)
+        if (P[p][i] != -1 && P[p][i] != P[q][i])
+            p = P[p][i], q = P[q][i];
+
+    return T[p];
+}
+
+void lca_init(int N)
+{
+    memset (P,-1,sizeof(P)); //শুরুতে সবগুলো ঘরে -১ থাকবে
+    int i, j;
+    for (i = 1; i <=N; i++)
+        P[i][0] = T[i];
+
+    for (j = 1; (1 << j) < N; j++)
+        for (i = 1; i <=N; i++)
+            if (P[i][j - 1] != -1)
+                P[i][j] = P[P[i][j - 1]][j - 1];
+}
+
+
+
+
+
+
+
 int main()
 {
     //READ("input.txt");
     //WRITE("output.txt");
+    input();
+    for(int i=1; i<=node; i++)
+        if(vis[i]==0)
+            dfs(i,i,0);
+    lca_init(node);
+
+
     return 0;
 }
 
